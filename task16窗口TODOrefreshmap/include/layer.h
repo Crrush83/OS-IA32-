4688a1dcdc50cdc7eb5d07e718cfd55eafcf8128
wczxt <1161953131@qq.com>
2022-02-11 06:32:55 +0800

    修好了鼠标不动的问题但不知道为什么

---
+++ task16窗口TODOrefreshmap/include/layer.h
@@ -0,0 +1,42 @@
+/*layer manager*/
+#include "memman.h"
+#include "graphic.h"
+#include "stdio.h"
+#ifndef _L_H
+#define _L_H
+#define MAX_LAYER_NUM 256
+#define LAYER_USE 1
+/*size:4+16+12 = 32*/
+struct LAYER{
+    //value 部分
+    unsigned char * buf;//图层的像素颜色
+    int bxsize,bysize,vx0,vy0;//b-block v - vram
+    int col_inv,height,flags;//
+};
+/*size:4+4+256*32 + 256*4  = 8KB+1KB+8B*/ //memman需要手动分配 有了memman之后利用其就可！
+struct LAYERMAN
+{
+    struct BOOTINFO * binfo;
+    int top;//最上层图层的高度？
+    struct LAYER layerset[MAX_LAYER_NUM];
+    struct LAYER *ordered_layers[MAX_LAYER_NUM];
+};
+struct LAYERMAN *layman;
+/*
+memman alloc
+*/
+struct LAYERMAN* layerman_init(struct MEMMAN *memman,struct BOOTINFO *binfo);
+struct LAYER *layer_register(struct LAYERMAN *layman);
+void layer_setbuf(struct LAYER *layer, unsigned char *buf, int xsize, int ysize, int col_inv);
+void layer_updown(struct LAYERMAN *layman, struct LAYER *layer, int height);
+void layers_refresh(struct LAYERMAN *layman);
+void layer_slide(struct LAYERMAN *layman, struct LAYER *layer, int vx0, int vy0);
+void layer_free(struct LAYERMAN *layman, struct LAYER *layer);
+
+struct LAYER* layer_screen(int x, int y);
+struct LAYER* layer_mouse(int x,int y);
+void layers_refresh_v2(struct LAYERMAN *layman, int vx0, int vy0, int vx1, int vy1);
+void layers_refresh_v3(struct LAYERMAN *layman,int h0, int vx0, int vy0, int vx1, int vy1);
+struct LAYER* layer_window(int posix,int posiy,int sizex,int sizey);
+struct LAYER* layer_time(void);
+#endif
---
+++ task16窗口TODOrefreshmap/os.c
@@ -0,0 +1,91 @@
+#include "io.h"
+#include "interupt.h"
+#include "time.h"
+#include "keyboardmouse.h"
+#include <stdio.h>
+#include "fifo.h"
+#include "memman.h"
+#include "graphic.h"
+#include "layer.h"
+struct BOOTINFO *binfo;
+struct MEMMAN *memman;
+struct INTERUPT_GATE_DESCRIPTOR *idt;
+int* refreshmap[200][320];
+int mousex = 0,mousey = 0;
+struct LAYER *timelayer;
+int main(void){
+  binfo = (struct BOOTINFO*)0x0ff0;
+//内存分布图0x0ff0开始应该顺次填入BOOTINFO的内容
+  memman= (struct MEMMAN *)MEMMAN_ADDR;
+  idt = (struct INTERUPT_GATE_DESCRIPTOR*)0x17e00;
+  char line[40];//debug
+  unsigned char keybuf[32];
+  unsigned char mousebuf[32];
+  extern struct FIFO8 *keyfifo; 
+  fifo8_init(keyfifo,32,keybuf); 
+  extern struct FIFO8 *mousefifo; 
+  fifo8_init(mousefifo,32,mousebuf);
+  struct MOUSE_DEC mdec;
+  init_mouse_decode(&mdec);//原来的位置
+  /*memmory managment*/
+
+  memman_init(memman);
+  int memsize = memtest(0x120000, 0x211ffff)/(1024*1024);//2000000B = 0x20MB = 32MB//怎么输出得48
+  sprintf(line,"%d MB",memsize); 
+  debugPrint((unsigned char *)line);
+    //向memman注册32MB
+  memman_free(memman,0x120000,1024*1024*32);
+
+	init_palette();
+  //debugPrint("why Broken");
+  /*layer managment*/
+  extern struct LAYERMAN *layman; 
+  layman = layerman_init(memman,binfo);
+  layer_screen(320,200);
+  layer_window(20,20,200,100);
+  timelayer = layer_time();
+  struct LAYER *mouselayer = layer_mouse(16,16);
+  
+  int i;
+
+  io_cli();
+	init_idt();
+  install_clock_int();
+  set_intgatedesc(idt+0x74,(int)int0x74,0x30,0x8e00); 
+  //install keyboard int
+  set_intgatedesc(idt+0x21,(int)int0x21,0x30,0x8e00); 
+  initRTC();
+  init8259A();
+  init_keyboard();// 键盘OK 
+  enable_mouse();//单独enable mouse mouse是可以了键盘却关了
+//既然是跳过去执行 应该有执行权限
+  io_sti();
+ 		for (;;) {
+       io_cli();
+       if(fifo8_status(keyfifo) + fifo8_status(mousefifo) > 0){
+         if(fifo8_status(mousefifo) > 0){
+        //get data then clean int 不论使用什么样的类型标识 只是影响打印的值 存储的二进制不会变化
+        unsigned char mousedata = fifo8_get(mousefifo);
+         io_sti();
+         if(mouse_decode(&mdec,mousedata)!=0){
+          // char *tmp = "         ";
+          // sprintf(tmp,"x:%d y:%d",mdec.x,mdec.y);
+          // debugPrint((unsigned char*)tmp);
+          move_mouse(binfo,&mdec,&mousex,&mousey);
+          layer_slide(layman,mouselayer,mousex,mousey);
+          }
+        }
+         else{
+        //  char *tmp = "    ";
+        //  sprintf(tmp,"%d",fifo8_get(keyfifo));
+        //  debugPrint((unsigned char *)tmp);
+        fifo8_get(keyfifo);
+         io_sti();
+         }
+       }else{
+        io_sti();
+        io_hlt();
+       }
+}
+return 0;
+}
